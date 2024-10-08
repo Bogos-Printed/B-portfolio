4b63b3a6f4b3057f16316468ea93824ef13e4c6f
Bogos-Printed <beauclair.nat@gmail.com>
2024-10-08 17:08:05 +0200

    feat: remove orbital controls

--- src/views/three.js
+++ src/views/three.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; 
 import fish from '../assets/models/fish.glb';
 
 export function initThreeJS(container) {
@@ -8,14 +8,15 @@ export function initThreeJS(container) {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2, false);
+  const renderer = new THREE.WebGLRenderer( {alpha: true});
+  renderer.setClearColor( 0x000000, 0 );
+  renderer.setSize(window.innerWidth / 2.5, window.innerHeight / 2.5, false);
   container.appendChild(renderer.domElement);
 
-  const controls = new OrbitControls(camera, renderer.domElement);
+  // const controls = new OrbitControls(camera, renderer.domElement);
   camera.position.set(3, 0, 0);
   camera.lookAt(0, -0.2, 0);
-  controls.update();
+  // controls.update();
 
   const loader = new GLTFLoader();
 
@@ -44,6 +45,6 @@ export function initThreeJS(container) {
   const light = new THREE.AmbientLight(0xffffff, 1);
   scene.add(light);
 
-  const hlp = new THREE.AxesHelper(1);
-  scene.add(hlp);
+  // const hlp = new THREE.AxesHelper(1);
+  // scene.add(hlp);
 }
