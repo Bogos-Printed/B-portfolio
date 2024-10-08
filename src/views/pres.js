a92bd05451d2bdfc075ffdb56bbe4ccedd5b2797
Bogos-Printed <beauclair.nat@gmail.com>
2024-10-08 10:45:27 +0200

    feat: add presentation section

---
+++ src/views/pres.js
@@ -0,0 +1,33 @@
+export default () => (
+  `
+  <section>
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 bg-black">
+      <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+        <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+          <img
+            alt=""
+            src="https://bestlifeonline.com/media-library/weird-dog-lizard-hybrid-image-funny-stock-photos.jpg?id=53396355&width=1245&height=700&quality=90&coordinates=0%2C80%2C0%2C80"
+            class="absolute inset-0 h-full w-full object-cover"
+          />
+        </div>
+
+        <div class="lg:py-24">
+          <h2 class="text-3xl text-white font-bold sm:text-4xl">My spaceship</h2>
+          <h2 class="text-3xl text-white font-bold sm:text-4xl">Got a tracktor beam</h2>
+          <a
+            href="#"
+            class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+          >
+            Contact
+          </a>
+          <a
+            href="#"
+            class="mt-8 inline-block rounded border border-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+          >
+            Projects
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  `);
